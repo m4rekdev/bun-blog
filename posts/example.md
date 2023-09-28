402376db5d35dc62ccfaf5f7e36a40a73cced822
losing <59799716+losingtop@users.noreply.github.com>
2023-09-28 21:50:51 +0200

    actually adding the new config options (oops)

---
+++ posts/example.md
@@ -0,0 +1,16 @@
+---
+title: Example Post
+coverImage: https://upload.wikimedia.org/wikipedia/en/4/4d/Shrek_%28character%29.png
+author: marekk
+draft: false
+pubDate: "September 28, 2023 7:42:00 PM"
+editedDate: null
+description: this is just an example. also shrek is cool
+tags:
+- shrek
+- example
+---
+
+# shrek
+
+shrek is cool
--- src/index.js
+++ src/index.js
@@ -28,7 +28,11 @@ const server = Bun.serve({
         //if it's an html file, replace the variables with the values in config.json
         if (filePath.match(/^(.(.*\.html))*$/)?.length)
             return new Response(
-                (await file.text()).replaceAll('{{ siteName }}', config.siteName),
+                (
+                    await file.text())
+                        .replaceAll('{{ siteName }}', config.siteName)
+                        .replaceAll('{{ siteDescription }}', config.siteDescription)
+                        .replaceAll('{{ siteHostname }}', config.siteHostname),
                 { headers: htmlHeaders }
             );
 
@@ -45,6 +49,10 @@ async function returnError(code) {
         return new Response(`Couldn't find the error page. You shouldn't be here. (☉_☉) Anyways, the status code is ${code}.`, { status: 404 });
 
     return new Response(
-        (await errorPage.text()).replaceAll('{{ siteName }}', config.siteName),
+        (
+            await errorPage.text())
+                .replaceAll('{{ siteName }}', config.siteName)
+                .replaceAll('{{ siteDescription }}', config.siteDescription)
+                .replaceAll('{{ siteHostname }}', config.siteHostname),
         { headers: htmlHeaders, status: code });
 }
